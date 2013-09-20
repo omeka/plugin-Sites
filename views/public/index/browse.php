a8950cd9ab0ab720cd4333c55658c8fad2f72fc2
Kim Nguyen <design@kimisgold.com>
2013-09-20 10:23:52 -0400

    Add body tags, reorganize single site in browse view, change line endings.

--- views/public/index/browse.php
+++ views/public/index/browse.php
@@ -1,25 +1,15 @@
 <?php
-
-$bodyclass = 'page sites-browse';
-echo head(array('title' => __('Browse sites') , 'bodyclass' => $bodyclass)); 
+echo head(array('title' => __('Browse sites') , 'bodyclass' => 'sites browse')); 
 ?>
 <div id="primary">
 
 <?php foreach(loop('site') as $site) : ?>
     <div class='sites-site'>
     <h2><?php echo  $site->title; ?></h2>
+    <p>(<?php echo sites_link_to_original_site($site, 'Original url'); ?>)</p>
     <?php echo sites_site_logo($site); ?>
-    <div class='sites-sample'>
-        <?php if($random_item = sites_random_site_item($site)): ?>
-                <p>Example Item:</p>
-                <h3><?php echo link_to_item(null, array(), 'show',  $random_item); ?></h3>
-                <?php echo item_image('square_thumbnail', array(), 0, $random_item); ?>
-                <p><?php echo link_to($site, 'show', 'Explore in the Commons'); ?></p>
-        <?php endif; ?>
-    </div>
     <p><?php echo $site->description; ?></p>
-    <p><?php echo sites_link_to_original_site($site); ?>
-
+    <p class="items"><span class="number"><?php echo metadata($site, 'total items'); ?></span> items</p>
     </div>
 <?php endforeach; ?>
 </div>

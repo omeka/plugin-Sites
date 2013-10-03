4609f41a693a27580c0ba77f451aa4ca949a011d
patrickmj <patrickmjchnm@gmail.com>
2013-10-03 14:08:10 -0400

    fix old use of __v()

--- helpers/ContextFunctions.php
+++ helpers/ContextFunctions.php
@@ -3,7 +3,7 @@
 function sites_set_current_context($context)
 {
     $contextType = get_class($context);
-    __v()->$contextType = $context;
+    get_view()->$contextType = $context;
 }
 
 function sites_get_current_context($contextType = null)
@@ -12,7 +12,7 @@ function sites_get_current_context($contextType = null)
     if(!$contextType) {
         $contextType = sites_get_current_context_type();
     }
-    return __v()->$contextType;
+    return get_view()->$contextType;
 }
 
 function sites_get_contexts_for_loop($contextType)
@@ -20,18 +20,18 @@ function sites_get_contexts_for_loop($contextType)
     //not bothering with Omeka_Inflector because all the sites contexts just add 's'
     //see the controller showAction
     $pluralized = $contextType . 's';
-    return __v()->$pluralized;
+    return get_view()->$pluralized;
 }
 
 function sites_get_current_context_type()
 {
-    return __v()->currentContextType;
+    return get_view()->currentContextType;
 }
 
 function sites_loop_contexts($contextType)
 {
     $pluralized = $contextType . 's';
-    __v()->currentContextType = $contextType;
+    get_view()->currentContextType = $contextType;
     return loop_records($pluralized, sites_get_contexts_for_loop($contextType), 'sites_set_current_context');
 }
 
@@ -39,7 +39,7 @@ function sites_set_contexts_for_loop($contexts)
 {
     $contextType = get_class($contexts[0]);
     $pluralized = $contextType . 's';
-    __v()->$pluralized = $contexts;
+    get_view()->$pluralized = $contexts;
 }
 
 function sites_link_to_original_context($context = null, $text = null)
@@ -47,7 +47,9 @@ function sites_link_to_original_context($context = null, $text = null)
     if(!$context) {
         $context = sites_get_current_context();
     }
-
+    if(!$context) {
+        return;
+    }
     if(!$text) {
         $text = sites_context('title');
     }
@@ -60,7 +62,9 @@ function sites_context($column, $options=array(), $context = null)
     if(!$context) {
         $context = sites_get_current_context();
     }
-
+    if(!$context) {
+        return;
+    }
     $returnText = html_escape($context->$column);
 
     if (isset($options['snippet'])) {
